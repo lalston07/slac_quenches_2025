bc0f9f4262e3d6e7e6f46d21b756c2300c3a42a7
lalston07 <leilaalston07@gmail.com>
2025-07-11 18:13:49 -0400

    Add files via upload

--- quench_frequency_plot.py
+++ quench_frequency_plot.py
@@ -35,19 +35,49 @@ timestamp_index = pd.Series(timestamps)         # pandas series object
 # .value_counts() - counts number of times each value appears 
 # .sort_index() - sorts results in chronological order
 quench_frequency = timestamp_index.dt.to_period("M").value_counts().sort_index()        
-print(quench_frequency)
 
 # step four - create full date range including skipped months
+full_range = pd.period_range(start=timestamp_index.min(), end=timestamp_index.max(), freq='M')
+quench_frequency = quench_frequency.reindex(full_range, fill_value=0)
+print(quench_frequency)
 
-# step five - plot the quench frequency data
+# step five - plot the quench frequency data as bar chart
 # x-axis is months and y-axis is number of quench files per month
+# plt.figure(figsize=(12,6))
+# ax = quench_frequency.plot(kind='bar', color='blue', label = f"CM: {CM_name}, CAV: {CAV_name}") 
+# ax.set_xlabel("Month", fontsize=14)
+# ax.set_ylabel("Number of Quenches", fontsize=14)
+# ax.set_title("Quench Frequency Over Time", fontsize=14)
+# ax.grid(True)  
+# ax.set_axisbelow(True) # makes grid go to back
+# ax.legend() # adds legend for cryomodle and cavity number
+# plt.tight_layout()
+# plt.show()
+
+# step six - plot the quench frequency data as scatter plot
 plt.figure(figsize=(12,6))
-ax = quench_frequency.plot(kind='bar', color='blue', label = f"CM: {CM_name}, CAV: {CAV_name}") 
-ax.set_xlabel("Month", fontsize=14)
-ax.set_ylabel("Number of Quenches", fontsize=14)
-ax.set_title("Quench Frequency Over Time", fontsize=14)
-ax.grid(True)  
-ax.set_axisbelow(True) # makes grid go to back
-ax.legend() # adds legend for cryomodle and cavity number
+x = quench_frequency.index.astype(str)
+    # quench_frequency.index gives the index of the Series (monthly)
+    # .astype(str) converts the period object into a string
+y = quench_frequency.values
+    # quench_frequency_values extracts the data vaules from the series (counts of quenches per month)
+plt.scatter(x, y, color = 'blue', label = f"CM: {CM_name}, CAV: {CAV_name}")
+
+# for file in []:
+#     if real_quench in file:
+#         plt.scatter(x, y, color = 'blue', label = f"Real Quenches")
+#     else:
+#         plt.scatter(x, y, color = 'red', label = f"Fake Quenches")
+
+plt.xlabel("Month", fontsize=14)
+plt.ylabel("Number of Quenches", fontsize=14)
+plt.title("Quench Frequency Over Time", fontsize=14)
+
+# plt.title(f"Quench Frequency Over Time in CM: {CM_name}, CAV: {CAV_name}", fontsize=14)
+
+plt.xticks(rotation=90)
+plt.grid(True, linewidth=0.5)
+plt.gca().set_axisbelow(True)
+plt.legend()
 plt.tight_layout()
 plt.show()
